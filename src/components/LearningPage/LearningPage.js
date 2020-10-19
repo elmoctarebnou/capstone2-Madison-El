3e7e554a121c6b97b0a44491caeeae9276ec94e8
El Moctar Ebnou <ebnoumoctar@gmail.com>
2020-10-18 22:57:28 -0500

    Next word button

--- src/components/LearningPage/LearningPage.js
+++ src/components/LearningPage/LearningPage.js
@@ -12,10 +12,14 @@ export default class LearningPage extends React.Component{
       frenchWords: [],
       total_score: 0,
       fetchOK: false,
-      guess: ''
+      guess: '',
+      next: null
     }
   }
   componentDidMount = () => {
+    this.fetch()
+  }
+  fetch = () => {
     return fetch(`${config.API_ENDPOINT}/language`, {
       method: "GET",
       headers:{
@@ -35,32 +39,42 @@ export default class LearningPage extends React.Component{
         'content-type': 'application/json'
       },
       body: JSON.stringify({guess: this.state.guess}) 
-    });
+    })
+    .then(res => res.json())
+    .then(res => this.setState({next: res}))
 
+  }
+  handleNextWord = async (event) => {
+    event.preventDefault()
+    await this.fetch();
+    this.setState({next: null});
   }
 
 
   render = () => {
     const word = this.state.fetchOK ? this.state.frenchWords[0].original : '';
     let learningPageHTML;
-    if(this.state.fetchOK === true){
       learningPageHTML = <>
       <h2>{this.state.language.name}</h2>
       <h2>Total correct answers: {this.state.language.total_score}</h2>
       <main>
           <div>
-            <h2>Translate the word:</h2>
+          <h2>Translate the {this.state.language.name} word to English:</h2>
             <span>{word}</span>
           </div>
-          <form className='guess-form' onSubmit={this.handleSubmit}>
-            <input onChange = {(event) => this.setState({guess: event.currentTarget.value})}type='text' placeholder='Enter your guess' required/>
-            <button>Submit</button>
-          </form>
+          {this.state.next
+          ? <>
+          <h1>{this.state.next.isCorrect ? <span className='correct'>Correct Answer</span> : <span className='incorrect'>Incorrect Answer</span>}</h1>
+          <h2>Translation: {this.state.next.answer}</h2>
+          <button onClick={this.handleNextWord} type='submit'>Try another word</button>
+          </>
+          : <form className='guess-form' onSubmit={this.handleSubmit}>
+          <input onChange = {(event) => this.setState({guess: event.currentTarget.value})}type='text' placeholder='Enter your guess' required/>
+          <button>Submit</button>
+        </form>}
       </main>
     </>
-    } else {
-      learningPageHTML = <h1>Loading...</h1>
-    }
+    
     return (learningPageHTML)
   }
 }
