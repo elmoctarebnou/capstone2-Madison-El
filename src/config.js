a7925bf279a7a478a031c6dac6fe3c1a20ec9709
El Moctar Ebnou <ebnoumoctar@gmail.com>
2020-10-28 23:42:13 -0500

    bug fix

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  API_ENDPOINT: 'https://salty-coast-02670.herokuapp.com/api',
+  API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }
+// https://salty-coast-02670.herokuapp.com/api
